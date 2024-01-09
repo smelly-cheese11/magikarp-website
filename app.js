e4a148c8a3ec0a7dac5f9875430b120374b73901
Caleb <126200524+smelly-cheese11@users.noreply.github.com>
2024-01-09 14:43:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    gsap.to('.dots',{
+        yPercent: 60,
+        scrollTrigger: {
+            scrub: 0.3,
+        }
+    })
+    gsap.to('.dots2',{
+        xPercent: 100,
+        scrollTrigger: {
+            scrub: 0.3,
+        }
+    })
+    
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.bg',
+            pin: true,
+            starts: 'top top',
+            end: '+=' + window.innerHeight + 2,
+            scrub: 0.3,
+        }
+    })
+
+    tl.to('.bg', {
+        rotation: -360,
+        xPercent: -100,
+        yPercent: 25,
+        backgroundColor: 'transparent',
+        backgroundSize: '100%',
+        clipPath: 'circle at 50% 50%'
+    })
+
+    tl.to('.bg', {
+        yPercent: 10,
+    })
+
+    tl.to('.bg', {
+        yPercent: 0,
+        xPercent: -10,
+        rotation: '+=10',
+    })
+})
